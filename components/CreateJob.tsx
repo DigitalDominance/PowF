ff7b35d8346763e0a5dacab05eaf33b16ed1f74a
DigitalDominance <digitaldominanceseo@gmail.com>
2025-05-31 11:21:48 -0400

    Update CreateJob.tsx

--- components/CreateJob.tsx
+++ components/CreateJob.tsx
@@ -49,8 +49,7 @@ export function CreateJob() {
         totalPayWei,
         formData.title,
         formData.description,
-        formData.numPositions,
-        { value }
+        formData.numPositions
       );
 
       toast({
