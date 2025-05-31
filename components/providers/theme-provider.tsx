ccaf79ead413bd5cd2093085a45e9bfb1de4c87d
DigitalDominance <digitaldominanceseo@gmail.com>
2025-05-31 12:37:23 -0400

    Add files via upload

---
+++ components/providers/theme-provider.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+type ThemeProviderProps = {
+  children: any
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+  disableTransitionOnChange?: boolean
+}
+
+export function ThemeProvider({
+  children,
+  attribute = "class",
+  defaultTheme = "system",
+  enableSystem = true,
+  disableTransitionOnChange = false,
+}: ThemeProviderProps) {
+  return (
+    <NextThemesProvider
+      attribute={attribute}
+      defaultTheme={defaultTheme}
+      enableSystem={enableSystem}
+      disableTransitionOnChange={disableTransitionOnChange}
+    >
+      {children}
+    </NextThemesProvider>
+  )
+} 
